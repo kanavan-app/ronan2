4bf8149ae960f76371d2122078cc679b33b63e53
LAPTOP-HQ82GS0B\ckwong <kanavan.app@gmail.com>
2021-05-20 22:55:53 +0800

    install dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const NODE_ENV = process.env.NODE_ENV;
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: !NODE_ENV ? '.env.development.local' : '.env.' + NODE_ENV,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
       database: 'ronan',
       entities: [],
       synchronize: true,
