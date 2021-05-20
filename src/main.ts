98fcbfe49a1616bf5062b94b458b72578f17d782
LAPTOP-HQ82GS0B\ckwong <kanavan.app@gmail.com>
2021-05-20 23:56:34 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const httpsOptions = {
@@ -17,6 +18,13 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ http2: true, https: httpsOptions }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Users API example')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'ck', description: 'Username for login' })
   username: string;
+
+  @ApiProperty({ example: '123456', description: 'Password for login' })
   password: string;
 }
